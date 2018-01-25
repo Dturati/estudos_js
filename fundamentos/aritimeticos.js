51e7089db37caa46d7c2b60e8e614e01aff48410
Dturati <davidturati@gmaim.com>
2018-01-25 13:13:49 -0300

    aritmetica

---
+++ fundamentos/aritimeticos.js
@@ -0,0 +1,9 @@
+const [a,b,c,d] = [3,4,1,15]
+
+const soma = a + b + c  + d 
+const subtracao = a - b -c - d
+const multiplicaca  = a * b 
+const divisao = a / b 
+const modulo = a % 2
+
+console.log(modulo)
